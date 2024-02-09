b08a1669b49591c0f053d8e62519e39996ee454f
LINUSOBURA <linusobura@gmail.com>
2024-02-09 11:19:23 +0300

    implemented task 4; Dict to instance

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,19 @@ class BaseModel:
     created_at = datetime.now()
     updated_at = datetime.now()
     
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         self.id = BaseModel.id
-        self.created_at = BaseModel.created_at
-        self.updated_at = BaseModel.updated_at
-            
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.created_at = BaseModel.created_at
+            self.updated_at = BaseModel.updated_at
+        
     
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -22,7 +30,7 @@ class BaseModel:
     def save(self):
         '''Method to update the public instance attribute "updated_at"
         With the current datetime'''
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         '''Method to generate a dictionary representation of the instance BaseModel'''
@@ -32,3 +40,5 @@ class BaseModel:
         instace_dict['created_at'] = datetime.isoformat(self.created_at)
         instace_dict['updated_at'] = datetime.isoformat(self.updated_at)
         return instace_dict
+
+        
